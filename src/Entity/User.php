be73819ce3124fc4b02c94b927535b041b041f04
lola200000 <mirzoyanlola@gmail.com>
2025-12-15 11:58:06 +0100

    Initial commit

---
+++ src/Entity/User.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+#[ORM\Entity]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(unique: true)]
+    private string $email;
+
+    #[ORM\Column]
+    private string $password;
+
+    #[ORM\Column]
+    private bool $isVerified = false;
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
+    }
+
+    public function getRoles(): array
+    {
+        return ['ROLE_USER'];
+    }
+    public function getPassword(): ?string
+{
+    return $this->password;
+}
+
+
+public function eraseCredentials(): void
+{
+}
+
+public function isVerified(): bool
+{
+    return $this->isVerified;
+}
+
+public function setIsVerified(bool $isVerified): static
+{
+    $this->isVerified = $isVerified;
+
+    return $this;
+}
+}
